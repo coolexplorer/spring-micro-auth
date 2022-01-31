b0f7b2a77904008bf82106274dae341f71a80332
seunkim <seunkim@ea.com>
2022-01-30 22:26:39 -0800

    [Fixed exception response issue] - Added Bad Credential exception message

--- src/main/java/io/coolexplorer/auth/service/impl/AuthServiceImpl.java
+++ src/main/java/io/coolexplorer/auth/service/impl/AuthServiceImpl.java
@@ -10,6 +10,7 @@ import io.coolexplorer.auth.service.AccountService;
 import io.coolexplorer.auth.service.AuthService;
 import io.coolexplorer.auth.service.JwtTokenMessageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -19,6 +20,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class AuthServiceImpl implements AuthService {
@@ -35,12 +37,14 @@ public class AuthServiceImpl implements AuthService {
             throw new UsernameNotFoundException(errorMessageSourceAccessor.getMessage(ExceptionCode.USERNAME_NOT_FOUND.getMessageKey()));
         }
 
-        if (passwordEncoder.matches(password, account.getPassword())) {
+        if (!passwordEncoder.matches(password, account.getPassword())) {
             throw new BadCredentialsException(errorMessageSourceAccessor.getMessage(ExceptionCode.BAD_CREDENTIALS.getMessageKey()));
         }
 
         account.setJwtToken(jwtTokenProvider.createJwtToken(account));
         account.setLastLogin(LocalDateTime.now());
+
+        LOGGER.debug("Account : {}", account);
 
         return accountService.update(account);
     }
